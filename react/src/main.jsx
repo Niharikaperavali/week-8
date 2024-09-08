4d7511df21b40c55628598b26bccffad99613c1c
Niharikaperavali <peravaliniharika@gmail.com>
2024-09-08 11:03:50 +0530

    Add files via upload

---
+++ react/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ShoppingList from './project.jsx';                //MyApp1.jsx,MyApp2.jsx,MyApp3.jsx,profile.jsx,project.jsx,ShoppingList.jsx
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ShoppingList />
+  </React.StrictMode>
+);
